1b3d97db94807700fe3d8acd4eae3d4d59bef96f
Steven Velez <stevendvelez9@gmail.com>
2020-12-15 19:15:27 -0500

    first commit; project to learn how to build e-commerce on react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+
+import {NavBar, Products} from "./components";
+import {commerce} from "./lib/commerce";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    const {data} = await commerce.products.list();
+
+    setProducts(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Products products={products}/>
     </div>
   );
 }
